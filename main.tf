0ce9c25f53941993f1ab4d33c77b40a93ec2c155
JLance7 <82004410+JLance7@users.noreply.github.com>
2023-02-18 14:41:19 -0600

    fix

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 }
 
 resource "aws_security_group" "blog" {
-  name = "blog"
+  name        = "blog"
   description = "Allow http and https in and everything out"
 
   vpc_id = data.aws_vpc.default.id
@@ -40,31 +40,31 @@ resource "aws_security_group" "blog" {
 
 //rules
 resource "aws_security_group_rule" "blog_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "http"
-  cirdr_blocks = [0.0.0.0/0]
+  type         = "ingress"
+  from_port    = 80
+  to_port      = 80
+  protocol     = "http"
+  cirdr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "http"
-  cirdr_blocks = [0.0.0.0/0]
+  type         = "ingress"
+  from_port    = 443
+  to_port      = 443
+  protocol     = "http"
+  cirdr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  type = "engress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cirdr_blocks = [0.0.0.0/0]
+  type         = "engress"
+  from_port    = 0
+  to_port      = 0
+  protocol     = "-1"
+  cirdr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
