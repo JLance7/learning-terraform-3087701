c31c1f6fa73032d21173d39eaa157e410b319186
JLance7 <82004410+JLance7@users.noreply.github.com>
2023-02-18 14:44:58 -0600

    fix again

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_groups_ids = [aws_security_group.blog.ig]
+  vpc_security_group_ids = [aws_security_group.blog.ig]
 
   tags = {
     Name = "HelloWorld"
