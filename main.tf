c2cb0bf55f0f19b8f4b7eb6194d06de4768e0f5f
JLance7 <82004410+JLance7@users.noreply.github.com>
2023-02-18 16:54:15 -0600

    load balancer

--- main.tf
+++ main.tf
@@ -16,6 +16,20 @@ data "aws_ami" "app_ami" {
 }
 
 
+//resources
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+  subnet_id     = module.blog_vpc.public_subnets[0]
+
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
 //modules
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -48,16 +62,45 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
 
-//resources
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  subnet_id     = module.blog_vpc.public_subnets[0]
+  name = "blog-alb"
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  load_balancer_type = "application"
+
+  vpc_id             = "module.blog_vpc.vpc_id"
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.target_id
+          port = 80
+        }
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
-    Name = "HelloWorld"
+    Environment = "dev"
   }
 }
+
+
+
