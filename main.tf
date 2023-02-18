f8674d18c3b2f28025e413c55f51402432724fc8
JLance7 <82004410+JLance7@users.noreply.github.com>
2023-02-18 15:57:10 -0600

    fix

--- main.tf
+++ main.tf
@@ -15,22 +15,9 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-//module
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.17.1"
-  name = "blog_new"
 
-  vpc_id =module.vpc.public_subnets[0]
-
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-module "vpc" {
+//modules
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -47,10 +34,26 @@ module "vpc" {
   }
 }
 
-//resource
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.1"
+  name = "blog_new"
+
+  vpc_id = module.blog_vpc.public_subnets[0]
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+//resources
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  subnet_id     = module.blog_vpc.public_subnets[0]
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
