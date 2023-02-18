5ae66f7db796f57d7735fff98cca2d2902413daa
JLance7 <82004410+JLance7@users.noreply.github.com>
2023-02-18 15:37:10 -0600

    create dev vpc

--- main.tf
+++ main.tf
@@ -15,17 +15,13 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 //module
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
   name = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id =module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cird_blocks = ["0.0.0.0/0"]
@@ -34,6 +30,22 @@ module "blog_sg" {
   egress_cird_blocks = ["0.0.0.0/0"]
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 
 //resource
 resource "aws_instance" "blog" {
