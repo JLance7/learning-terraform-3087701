3eba6963a305042b82d4573bb30fa86b31337df0
JLance7 <82004410+JLance7@users.noreply.github.com>
2023-02-18 17:04:58 -0600

    asg

--- main.tf
+++ main.tf
@@ -17,17 +17,7 @@ data "aws_ami" "app_ami" {
 
 
 //resources
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  subnet_id     = module.blog_vpc.public_subnets[0]
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
 
 
 //modules
@@ -100,6 +90,20 @@ module "alb" {
   tags = {
     Environment = "dev"
   }
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.7.1"
+  name = "blog"
+
+  min_size = 1
+  max_size = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  image_id                 = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
 }
 
 
