b4725c0a2cb8324c76bdf33acc681d961ec5bc06
JLance7 <82004410+JLance7@users.noreply.github.com>
2023-02-18 17:42:18 -0600

    variables update

--- variables.tf
+++ variables.tf
@@ -2,3 +2,42 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for ami"
+
+  type = object({
+    name = string
+    owner = string
+  })
+
+  default = {
+    name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
+}
+
+variable "env" = {
+  description = "Development environment"
+  
+  type = object({
+    name = string
+    network_prefix = string
+  })
+
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "min_size" = {
+  description = "Minimum number of instances in ASG"
+  default = 1
+}
+
+variable "max_size" = {
+  description = "maximum number of instances in asg"
+  default = 2
+}
+
